5748665d5f509836adff8ca545e21e87258c5554
Sudarsan <sudar.theone@gmail.com>
2020-02-09 12:58:08 +0800

    added Start()

--- bot/bot.go
+++ bot/bot.go
@@ -3,8 +3,10 @@ package bot
 import (
 	"errors"
 	"fmt"
+	"image"
 	"image/color"
 	"io"
+	"math"
 	"os/exec"
 	"strconv"
 	"time"
@@ -105,4 +107,128 @@ func (f *Follower) init(cmd *exec.Cmd) error {
 	}()
 
 	return nil
+}
+
+// Start the drone.
+// Not threadsafe at all. Doesn't have to be.
+func (f *Follower) Start() {
+	frameSize := frameX * frameY * 3
+	refDistance := float64(0)
+	detected := false
+	left := float32(0)
+	top := float32(0)
+	right := float32(0)
+	bottom := float32(0)
+	var window = gocv.NewWindow("Tello")
+	var tracking = false
+	var detectSize = false
+	var distTolerance = 0.05 * dist(0, 0, frameX, frameY)
+
+	for {
+		outData := make([]byte, frameSize)
+		if _, err := io.ReadFull(f.ffmpegOut, outData); err != nil {
+			fmt.Println(err)
+			continue
+		}
+		img, err := gocv.NewMatFromBytes(frameY, frameX, gocv.MatTypeCV8UC3, outData)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		if img.Empty() {
+			continue
+		}
+		W := float32(img.Cols())
+		H := float32(img.Rows())
+
+		blob := gocv.BlobFromImage(img, 1.0, image.Pt(128, 96), gocv.NewScalar(104.0, 177.0, 123.0, 0), false, false)
+		defer blob.Close()
+
+		f.net.SetInput(blob, "data")
+
+		detBlob := f.net.Forward("detection_out")
+		defer detBlob.Close()
+		detections := gocv.GetBlobChannel(detBlob, 0, 0)
+		defer detections.Close()
+
+		for r := 0; r < detections.Rows(); r++ {
+			confidence := detections.GetFloatAt(r, 2)
+			if confidence < 0.5 {
+				continue
+			}
+
+			left = detections.GetFloatAt(r, 3) * W
+			top = detections.GetFloatAt(r, 4) * H
+			right = detections.GetFloatAt(r, 5) * W
+			bottom = detections.GetFloatAt(r, 6) * H
+
+			left = min(max(0, left), W-1)
+			right = min(max(0, right), W-1)
+			bottom = min(max(0, bottom), H-1)
+			top = min(max(0, top), H-1)
+
+			rect := image.Rect(int(left), int(top), int(right), int(bottom))
+			gocv.Rectangle(&img, rect, f.green, 3)
+			detected = true
+		}
+
+		window.IMShow(img)
+		if window.WaitKey(10) >= 0 {
+			break
+		}
+
+		if !tracking || !detected {
+			continue
+		}
+
+		if detectSize {
+			detectSize = false
+			refDistance = dist(left, top, right, bottom)
+		}
+
+		distance := dist(left, top, right, bottom)
+
+		if right < W/2 {
+			f.drone.CounterClockwise(50)
+		} else if left > W/2 {
+			f.drone.Clockwise(50)
+		} else {
+			f.drone.Clockwise(0)
+		}
+
+		if top < H/10 {
+			f.drone.Up(25)
+		} else if bottom > H-H/10 {
+			f.drone.Down(25)
+		} else {
+			f.drone.Up(0)
+		}
+
+		if distance < refDistance-distTolerance {
+			f.drone.Forward(20)
+		} else if distance > refDistance+distTolerance {
+			f.drone.Backward(20)
+		} else {
+			f.drone.Forward(0)
+		}
+	}
+
+}
+
+func dist(x1, y1, x2, y2 float32) float64 {
+	return math.Sqrt(float64((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)))
+}
+
+func min(a, b float32) float32 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b float32) float32 {
+	if a > b {
+		return a
+	}
+	return b
 }
